4a6e529aa2507f20d8780f5f087940cc2c37f175
Nagy Osama <nagyosama85@gmail.com>
2024-07-20 20:21:47 +0300

    mongo

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,9 @@ import  Mongoose  from "mongoose";
 
 export const db_connection = async () => {
   try {
-    await Mongoose.connect("mongodb://127.0.0.1:27017/book-store");
+    await Mongoose.connect(
+      "mongodb+srv://nagyosama85:010050079306Nn@bookstore.nv0z4wx.mongodb.net/?retryWrites=true&w=majority&appName=bookStore"
+    );
     console.log("connected to DataBase");
   } catch (error) {
     console.log("error in db connection", error);
